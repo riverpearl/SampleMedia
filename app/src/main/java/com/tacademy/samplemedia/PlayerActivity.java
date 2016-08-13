60836ad5e027ef900e55bb8e5a510a9da2dd0da8
Pearl River <riverpearl0115@gmail.com>
2016-08-14 00:38:40 +0900

    add seek bar

--- app/src/main/java/com/tacademy/samplemedia/PlayerActivity.java
+++ app/src/main/java/com/tacademy/samplemedia/PlayerActivity.java
@@ -1,15 +1,19 @@
 package com.tacademy.samplemedia;
 
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    SeekBar musicView;
     MediaPlayer mPlayer;
     PlayState pState;
 
@@ -24,13 +28,41 @@ public class PlayerActivity extends AppCompatActivity {
         RELEASED
     }
 
+    boolean isSeeking = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        musicView = (SeekBar)findViewById(R.id.seek_music);
         mPlayer = MediaPlayer.create(this, R.raw.run_and_run);
         pState = PlayState.PREPARED;
+
+        musicView.setMax(mPlayer.getDuration());
+        musicView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = -1;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser)
+                    this.progress = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                progress = -1;
+                isSeeking = true;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (progress != -1 && pState == PlayState.STARTED)
+                    mPlayer.seekTo(progress);
+
+                isSeeking = false;
+            }
+        });
 
         Button btn = (Button)findViewById(R.id.btn_start);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -69,8 +101,10 @@ public class PlayerActivity extends AppCompatActivity {
         }
 
         if (pState == PlayState.PREPARED || pState == PlayState.PAUSED) {
+            mPlayer.seekTo(musicView.getProgress());
             mPlayer.start();
             pState = PlayState.STARTED;
+            mHandler.post(progressRunnable);
         }
     }
 
@@ -85,6 +119,7 @@ public class PlayerActivity extends AppCompatActivity {
         if (pState == PlayState.STARTED || pState == PlayState.PAUSED) {
             mPlayer.stop();
             pState = PlayState.STOPPED;
+            musicView.setProgress(0);
         }
     }
 
@@ -101,4 +136,20 @@ public class PlayerActivity extends AppCompatActivity {
         mPlayer = null;
         pState = PlayState.RELEASED;
     }
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
+    Runnable progressRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (pState == PlayState.STARTED) {
+                if (!isSeeking) {
+                    int position = mPlayer.getCurrentPosition();
+                    musicView.setProgress(position);
+                }
+
+                mHandler.postDelayed(this, 100);
+            }
+        }
+    };
 }
